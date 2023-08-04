615d123a047084e807b7f71f0bd1dbd80e566a5d
PleasureAmara <140104356+PleasureAmara@users.noreply.github.com>
2023-08-04 18:33:31 +0300

    Update 1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,3 +2,7 @@ word = "Holberton"
 print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[7:9]))
 print("Middle word: {}".format(word[1:8]))
+word = "School"
+print("First 3 letters: {}".format(word[0:3]))
+print("Last 2 letters: {}".format(word[4:6]))
+print("Middle word: {}".format(word[1:6]))
